3b40f0ce864cb989f678cbe7582d4093b30954de
sugga-cloud <72293850+sugga-cloud@users.noreply.github.com>
2025-09-23 12:44:55 +0530

    ;'

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
-  authDomain: "yourtube-8cda9.firebaseapp.com",
-  projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.firebasestorage.app",
-  messagingSenderId: "921641878423",
-  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
+  apiKey: "AIzaSyBDz6_c9-AUG5hjEPWuMY8sBUZIWyxVUcY",
+  authDomain: "my-awesome-project-id-4eb67.firebaseapp.com",
+  projectId: "my-awesome-project-id-4eb67",
+  storageBucket: "my-awesome-project-id-4eb67.firebasestorage.app",
+  messagingSenderId: "53833416764",
+  appId: "1:53833416764:web:5410d5a2a5eea13f96c339"
 };
 
 // Initialize Firebase
