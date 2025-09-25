e5ea30e20b91e5351e48f4b31144fb01e2271875
sugga-cloud <72293850+sugga-cloud@users.noreply.github.com>
2025-09-25 10:58:30 +0530

    .../pointer

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL || 'https://nullclass-assignment-1serser.onrender.com/',
+  baseURL: process.env.BACKEND_URL || 'https://nullclass-assignment-1serser.onrender.com',
 });
 export default axiosInstance;
